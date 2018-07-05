b3d095a1bd3d1ff096bd7e40ce3ed05c12aaff7b
Cathrin <cathrin.kopfensteiner@gmail.com>
2018-07-05 12:44:40 +0200

    homework secret number

---
+++ secret_number_homework.py
@@ -0,0 +1,10 @@
+secret = 7
+
+guess = raw_input("Gib eine Zahl ein: ")
+
+while secret != int(guess):
+   guess = raw_input("Falsche Zahl, neue Eingabe: ")
+if int(guess) == secret:
+       print "Gratulation, geheime Zahl gefunden"
+
+7
